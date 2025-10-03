d7fe4ff4b7863a6e5ecdda0d24864ad9cfb727ce
Artur Zacniewski <a.zacniewski@gmail.com>
2025-10-03 22:46:26 +0200

    Add materials about basic OOP concepts

---
+++ Java/03/MainThird.java
@@ -0,0 +1,11 @@
+public class MainThird {
+
+    // metoda statyczna
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
