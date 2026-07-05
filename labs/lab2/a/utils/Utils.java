7c23050930b65e5fa8d6ea3dbb77c2911cd56469
Artur Zacniewski <a.zacniewski@gmail.com>
2026-07-05 13:04:06 +0200

    Remove old labs files

--- labs/lab2/a/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package laboratoria.lab2.a.utils;
-
-public class Utils {
-    public static String name() {
-        return "Utils from package A";
-    }
-}
--- labs/lab2/b/utils/Utils.java
+++
@@ -1,7 +0,0 @@
-package laboratoria.lab2.b.utils;
-
-public class Utils {
-    public static String name() {
-        return "Utils from package B";
-    }
-}
